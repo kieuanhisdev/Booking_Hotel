d29f1567a9eba1ba181eacc00ec9015426025166
Kieuanhdev <kieuanh.dev@gmail.com>
2025-05-01 22:26:13 +0700

    setup project

---
+++ src/test/java/vn/id/kieuanhdev/BookingHotel/BookingHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.kieuanhdev.BookingHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
